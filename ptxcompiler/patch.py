f3d09d9d14c25b4ccf4c4c9b5cf01d4bde6d2147
brandon-b-miller <brmiller@nvidia.com>
2022-10-19 09:49:07 -0700

    refactor patch.py

--- ptxcompiler/patch.py
+++ ptxcompiler/patch.py
@@ -13,9 +13,11 @@
 # limitations under the License.
 
 import logging
+import math
 import os
 import subprocess
 import sys
+import warnings
 
 from ptxcompiler.api import compile_ptx
 
@@ -26,13 +28,16 @@ required_numba_ver = (0, 54)
 
 try:
     import numba
+
     ver = numba.version_info.short
     if ver >= required_numba_ver:
         _numba_version_ok = True
     else:
-        _numba_error = (f"version {numba.__version__} is insufficient for "
-                        "ptxcompiler patching - at least "
-                        "%s.%s is needed." % required_numba_ver)
+        _numba_error = (
+            f"version {numba.__version__} is insufficient for "
+            "ptxcompiler patching - at least "
+            "%s.%s is needed." % required_numba_ver
+        )
 except ImportError as ie:
     _numba_error = f"failed to import Numba: {ie}."
 
@@ -74,8 +79,10 @@ def get_logger():
         if config.CUDA_LOG_LEVEL:
             # Create a simple handler that prints to stderr
             handler = logging.StreamHandler(sys.stderr)
-            fmt = ('== CUDA (ptxcompiler) [%(relativeCreated)d] '
-                   '%(levelname)5s -- %(message)s')
+            fmt = (
+                "== CUDA (ptxcompiler) [%(relativeCreated)d] "
+                "%(levelname)5s -- %(message)s"
+            )
             handler.setFormatter(logging.Formatter(fmt=fmt))
             logger.addHandler(handler)
         else:
@@ -102,11 +109,11 @@ class PTXStaticCompileCodeLibrary(CUDACodeLibrary):
             msg = "Cannot link multiple PTX files with forward compatibility"
             raise RuntimeError(msg)
 
-        arch = f'sm_{cc[0]}{cc[1]}'
-        options = [f'--gpu-name={arch}']
+        arch = f"sm_{cc[0]}{cc[1]}"
+        options = [f"--gpu-name={arch}"]
 
         if self._max_registers:
-            options.append(f'--maxrregcount={self._max_registers}')
+            options.append(f"--maxrregcount={self._max_registers}")
 
         # Compile PTX to cubin
         ptx = ptxes[0]
@@ -127,15 +134,10 @@ run_major, run_minor = cuda.runtime.get_version()
 print(f'{drv_major} {drv_minor} {run_major} {run_minor}')
 """
 
+logger = get_logger()
 
-def patch_needed():
-    # If Numba is not present, we don't need the patch.
-    # We also can't use it to check driver and runtime versions, so exit early.
-    if not _numba_version_ok:
-        return False
 
-    logger = get_logger()
-
+def patch_forced_by_user():
     # The patch is needed if the user explicitly forced it with an environment
     # variable.
     apply = os.getenv("PTXCOMPILER_APPLY_NUMBA_CODEGEN_PATCH")
@@ -146,9 +148,10 @@ def patch_needed():
         except ValueError:
             apply = False
 
-    if apply:
-        return True
+    return True if apply else False
 
+
+def check_disabled_in_env():
     # We should avoid checking whether the patch is needed if the user
     # requested that we don't check (e.g. in a non-fork-safe environment)
     check = os.getenv("PTXCOMPILER_CHECK_NUMBA_CODEGEN_PATCH_NEEDED")
@@ -161,18 +164,34 @@ def patch_needed():
     else:
         check = True
 
-    if not check:
+    return check == False
+
+
+def patch_needed():
+    # If Numba is not present, we don't need the patch.
+    # We also can't use it to check driver and runtime versions, so exit early.
+    if not _numba_version_ok:
         return False
+    if patch_forced_by_user():
+        return True
+    if check_disabled_in_env():
+        return False
+    else:
+        # Check whether the patch is needed by comparing the driver and runtime
+        # versions - it is needed if the runtime version exceeds the driver
+        # version.
+        driver_version, runtime_version = get_versions()
+        return driver_version < runtime_version
 
-    # Check whether the patch is needed by comparing the driver and runtime
-    # versions - it is needed if the runtime version exceeds the driver
-    # version.
-    cp = subprocess.run([sys.executable, '-c', CMD], capture_output=True)
 
+def get_versions():
+    cp = subprocess.run([sys.executable, "-c", CMD], capture_output=True)
     if cp.returncode:
-        msg = (f'Error getting driver and runtime versions:\n\nstdout:\n\n'
-               f'{cp.stdout.decode()}\n\nstderr:\n\n{cp.stderr.decode()}\n\n'
-               'Not patching Numba')
+        msg = (
+            f"Error getting driver and runtime versions:\n\nstdout:\n\n"
+            f"{cp.stdout.decode()}\n\nstderr:\n\n{cp.stderr.decode()}\n\n"
+            "Not patching Numba"
+        )
         logger.error(msg)
         return False
 
@@ -183,7 +202,24 @@ def patch_needed():
     logger.debug("CUDA Driver version %s.%s" % driver_version)
     logger.debug("CUDA Runtime version %s.%s" % runtime_version)
 
-    return driver_version < runtime_version
+    return driver_version, runtime_version
+
+
+def safe_get_versions():
+    if check_disabled_in_env():
+        try:
+            # allow user to specify driver/runtime versions manually, if necessary
+            driver_version = os.environ["PTXCOMPILER_KNOWN_DRIVER_VERSION"].split(".")
+            runtime_version = os.environ["PTXCOMPILER_KNOWN_RUNTIME_VERSION"].split(".")
+        except KeyError:
+            warnings.warn(
+                "No way to determine driver and runtime versions for patching, "
+                "set PTXCOMPILER_KNOWN_DRIVER_VERSION/PTXCOMPILER_KNOWN_RUNTIME_VERSION"
+            )
+            return math.inf, math.inf
+        else:
+            driver_version, runtime_version = get_versions()
+        return driver_version, runtime_version
 
 
 def patch_numba_codegen_if_needed():
